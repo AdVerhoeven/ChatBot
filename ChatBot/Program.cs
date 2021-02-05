5adbe95727df57745c34f91fdc458895318ac356
AdVerhoeven <adver94@gmail.com>
2021-02-05 12:20:31 +0100

    Add project files.

---
+++ ChatBot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path of a file to be used: ");
+            var filePath = Console.ReadLine();
+            var bot = new SimpleChatBot("English", filePath);
+
+            Console.WriteLine("Computer: I am a very simple chat bot.");
+            var exit = false;
+            while (!exit)
+            {
+                exit = Prompt(bot);
+            }
+        }
+
+        static bool Prompt(SimpleChatBot bot)
+        {
+            Console.Write("User: ");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Computer: {bot.RespondSimple(input)}");
+            return input == "bye";
+        }
+    }
+}
---
+++ ChatBot/SimpleChatBot.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ChatBot
+{
+    public class SimpleChatBot
+    {
+        public string Language { get; }
+        public Dictionary<string, string> ResponseDictionary { get; }
+
+        public SimpleChatBot(string lang, Dictionary<string, string> responseDict)
+        {
+            Language = lang;
+            ResponseDictionary = responseDict;
+        }
+        public SimpleChatBot(string lang, string filePath)
+        {
+            Language = lang;
+            ResponseDictionary = ResponseDictionaryFromFile(filePath);
+        }
+
+        /// <summary>
+        /// Attempt to read a response dictionary from a file.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private Dictionary<string, string> ResponseDictionaryFromFile(string filePath)
+        {
+            try
+            {
+                var sr = new StreamReader(filePath);
+                var dict = new Dictionary<string, string>();
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] kvp = line.Split(';');
+                    if (kvp.Length != 2)
+                    {
+                        throw new InvalidDataException($"Expected 2 arguments, got {kvp.Length} arguments");
+                    }
+                    dict.Add(kvp[0].ToLower(), kvp[1]);
+                }
+                return dict;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string RespondSimple(string input)
+        {
+            var key = TextOnly(input);
+            try
+            {
+                return ResponseDictionary[key];
+            }
+            catch (KeyNotFoundException e)
+            {
+                return "I do not know how to respond to this.";
+            }
+        }
+
+        public string TextOnly(string text)
+        {
+            text = text.ToLower().Trim();
+            var regex = new Regex(@"^([a-z]+\s)*$");
+            if (regex.IsMatch(text))
+            {
+                return text;
+            }
+            else
+            {
+                var matches = regex.Matches(text);
+                StringBuilder sb = new StringBuilder();
+                foreach (Match match in matches)
+                {
+                    sb.Append(match.Value);
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
